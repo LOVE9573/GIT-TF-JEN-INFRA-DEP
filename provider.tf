fe409b98c16520f942468f71ae8b817f742fb0c0
AAKASH GADLING <151997293+LOVE9573@users.noreply.github.com>
2024-10-21 00:03:02 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    TF_VAR_AWS_ACCESS_KEY_ID     = credentials('aws_access_key_id')  
-    TF_VAR_AWS_SECRET_ACCESS_KEY = credentials('aws_secret_access_key')
-    region                       = "us-east-1"
+  region     = "us-east-1"
+  access_key = "aws_access_key_id"
+  secret_key = "aws_secret_access_key"
 }
